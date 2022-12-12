68ab7d86da13066c1cbe09252fe4748bd6072d3e
cnagridge <118046231+cnagridge@users.noreply.github.com>
2022-12-12 15:07:35 -0500

    1

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,29 @@
 // Assignment code here
+const num = [1,2,3,4,5,6,7,8,9,0];
+const sym = ["@","#","$","%"];
+const characterCodes = Array.from(Array(26)).map((_, i)=>i+97);
+const lowercaseLetters = characterCodes.map(code=>String.fromCharCode(code));
+const uppercaseLetters = lowercaseLetters.map(letter=>letter.toUpperCase())
+const generatePassword = (length, hasNumbers, hasSymbols, hasLowercase, hasUppercase)=>{
+  const availableCharacters = [
+    ...(hasSymbols ? sym : []),
+    ...(hasNumbers ? num : []),
+    ...(hasUppercase ? uppercaseLetters : []),
+    ...(hasLowercase ? lowercaseLetters : [])
+  ];
 
+  let password = "";
 
+  if(availableCharacters.length === 0){
+    return "";
+  }
+
+  for(let i=0; i<length; i++){
+    const randomIndex = Math.floor(Math.random() * availableCharacters.length);
+    password += availableCharacters[randomIndex];
+  }
+  return password;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
